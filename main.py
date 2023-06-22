8b491c57f61eb0dfa59e741b24c95c8b27ff93e7
Siah2016 <jurellz02@gmail.com>
2023-06-22 13:35:20 -0400

    Updating website for scrape

--- main.py
+++ main.py
@@ -2,3 +2,17 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 
+# Product ID -
+# Name - 
+# Gender - 
+# Color - 
+# Price - 
+
+file = open("nike_shoes.csv", "w", newline="")
+writer = csv.writer(file)
+writer.writerow(["Name, Gender", "Color", "Price"])
+
+page = requests.get("https://www.nike.com/w/air-force-1-shoes-5sj3yzy7ok")
+soup = BeautifulSoup(page.text, "html.parser")
+
+product_id = soup.find_all()
